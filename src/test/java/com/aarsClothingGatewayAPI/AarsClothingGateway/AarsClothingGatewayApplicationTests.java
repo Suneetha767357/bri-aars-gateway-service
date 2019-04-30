75d0e440e9a07dca2b15080406c0fccc3b51cfc7
Suneetha <vineet.battula@cognizant.com>
2019-04-30 14:52:26 -0400

    first commit

---
+++ src/test/java/com/aarsClothingGatewayAPI/AarsClothingGateway/AarsClothingGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aarsClothingGatewayAPI.AarsClothingGateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AarsClothingGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
